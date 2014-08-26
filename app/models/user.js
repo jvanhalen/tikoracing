85fa65734bae2fbe1cd1ac179d29869aaa05aa71
jvanhalen <paakayttaja@gmail.com>
2014-08-26 20:40:22 +0300

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -19,22 +19,5 @@ userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
 
-// check if password is valid
-userSchema.methods.addNewUser = function(username, password) {
-
-	var User = mongoose.model('User', userSchema);
-	var user = new User({ local: {username: username, password: this.methods.generateHash(password)}});
-
-	user.save(function (err) {
-
-		if (err) {
-			console.log("got some mysterious mongodb error:", err);
-			return handleError(err);
-		}
-		// saved!
-		console.log("new user added!");
-	})
-};
-
 // create the model for users and expose it to the app
 module.exports = mongoose.model('User', userSchema);
